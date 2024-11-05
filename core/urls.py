4775192fa5891443e0717ce290831de675c076c0
Baracut Vlad <vladbaracut7@yahoo.com>
2024-11-05 22:33:47 +0200

    Login Page

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('getStarted/', views.get_started, name='get_started'),
+    path('login/', views.login, name='login')
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'core/home.html', {'products': products})
+
+def get_started(request):
+    return render(request, 'core/get_started.html')
+
+def login(request):
+    return render(request, 'core/login.html')
